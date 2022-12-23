f740927eed52c26fc218bef92802522c1af7b1f4
Mohini5395 <115691287+Mohini5395@users.noreply.github.com>
2022-12-23 16:54:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,21 @@
 
 let input = document.getElementById('input');
 let expression = "";
-function solve(e) {
-    // console.log(e)
-    if (e === 'ans') {
-        // evaluate the expression
+function solve(e){
+    if(e === 'ans') {
         let result = eval(expression);
         console.log(result);
         input.value = result;
     }
 
-    else if (e === 'clear') {
-        expression = "";
-        input.value = ""
+    else if( e === 'clear') {
+        expression = " ";
+        input.value = " "
     }
     else {
         expression += e;
         console.log(expression)
         input.value = expression;
+
     }
 }
